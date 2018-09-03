7c8effea1d4ac5ad700d17686510a29a57d500e4
Daniel Rosselló S <drossan@hotmail.es>
2018-09-03 10:16:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <?php
-    ob_start();
-    session_start();
-    // session_status() != PHP_SESSION_NONE ?? session_start();
+	ob_start();
+	session_status() === PHP_SESSION_NONE ? session_start() : false;
 
-    require_once __DIR__.'/core/init.php';
+	require_once __DIR__.'/core/init.php';
 
-    Rout::setRequest($_SERVER['REQUEST_URI']);
+	Rout::setRequest($_SERVER['REQUEST_URI']);
 	Rout::add('/', 'Index::index');
 	Rout::add('/home', 'Index::index');
 	Rout::add('/pagina/:pagina', 'Index::index');
-	
-    Rout::run();
-    
-    // ob_end_flush();
+
+	Rout::run();
+
+	ob_end_flush();
